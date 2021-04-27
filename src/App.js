00c50923c4c2d9ce2b5c15f05bd906cfb9f8f006
Blizzcane <blizzcane@gmail.com>
2021-04-26 19:55:25 -0500

    changes

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
 
 /**
- * App is a wrapper for <Layout>, you should not need to change this file. testing git from visual studio
+ * App is a wrapper for <Layout>, you should not need to change this file. 
  */
 
 function App() {
