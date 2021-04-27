b933f4c4255164585056ad1cf4ece03b8afcddae
Blizzcane <blizzcane@gmail.com>
2021-04-26 19:00:32 -0500

    adding comment to check git

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import Layout from "./Layout";
 
 /**
- * App is a wrapper for <Layout>, you should not need to change this file.
+ * App is a wrapper for <Layout>, you should not need to change this file. testing git
  */
 
 function App() {
