05c900e4e06ac7b32e0a662db4fff8509e8bb6d6
Blizzcane <blizzcane@gmail.com>
2021-04-26 21:49:19 -0500

    added loadDecks

--- src/Decks.js
+++ src/Decks.js
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
 import React from "react";
+import listDecks from "./utils/api/index.js";
 
 function Decks() {
-
-    const {decks, setDecks} = useState([]);
+  const { decks, setDecks } = useState([]);
+  const abortController = new AbortController();
   useEffect(() => {
-      
-  }, []);
+    async function loadDecks() {
+      try {
+        const response = await listDecks(abortController.signal);
+        setDecks(response);
+      } catch (error) {
+        if (error.name !== "AbortError") {
+          throw error;
+        }
+      }
+    }
 
+    loadDecks();
+
+    return () => abortController.abort();
+  }, []);
 
   return <h1>Hey this is the {decks}</h1>;
 }
