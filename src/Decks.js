ef3e4388cc2d6ea803a73e63c1c7b9066fcc432e
Blizzcane <blizzcane@gmail.com>
2021-04-26 20:59:59 -0500

    testing API

--- src/Decks.js
+++ src/Decks.js
@@ -1,11 +1,22 @@
+import { useEffect, useState } from "react";
 import React from "react";
 
-
-
 function Decks() {
-  return (
-      <h1>Hey this is the decks</h1>
-  );
+
+    const {decks, setDecks} = useState([]);
+  useEffect(() => {
+    async function loadDecks() {
+      const response = await fetch(API_BASE_URL);
+      const deck = await response.json();
+      console.log(deck);
+      setDecks(decks);
+    }
+
+    loadDecks();
+  }, []);
+
+
+  return <h1>Hey this is the {decks}</h1>;
 }
 
 export default Decks;
