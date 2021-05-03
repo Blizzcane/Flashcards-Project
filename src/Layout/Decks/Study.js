8c1ef1127445012df89880d4e1eecc7d77d5ac11
Blizzcane <blizzcane@gmail.com>
2021-05-02 21:16:17 -0500

    revamped layout

--- src/Layout/Decks/Study.js
+++ src/Layout/Decks/Study.js
@@ -1,27 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { readDeck } from "../../utils/api/index";
 import { Link, useParams } from "react-router-dom";
 import Cards from "../Cards/Cards"
 
-function Study({currentDeck, setCurrentDeck}) {
-  const { deckId } = useParams();
+function Study({currentDeck}) {
   
-
-  useEffect(() => {
-    const abortController = new AbortController();
-    async function deckSelected() {
-      const selectedDeck = await readDeck(deckId, abortController.signal);
-      setCurrentDeck(selectedDeck);
-    }
-
-    deckSelected();
-
-    return () => abortController.abort();
-  }, []);
-
-  const cards = currentDeck["cards"];
-  console.log(cards);
-
   return (
     <div>
       <nav>
@@ -36,7 +18,7 @@ function Study({currentDeck, setCurrentDeck}) {
         </ol>
       </nav>
       <h1>Study: {currentDeck["name"]}</h1>
-      <Cards />
+      <Cards currentDeck={currentDeck} />
     </div>
   );
 }
