224c2e35c190f2330e7f6d59373718d0ad0d9eb1
Blizzcane <blizzcane@gmail.com>
2021-05-03 21:20:14 -0500

    fixed Cards rendering

--- src/Layout/Cards/Cards.js
+++ src/Layout/Cards/Cards.js
@@ -2,20 +2,19 @@ import React from "react";
 
 //this will return the current card side.
 
-function Cards({cards, cardNum, setCardNum, flipped, setFlipped}) {
-   
-  console.log(cards[cardNum]);
-  return (
-    <div className="card mt-2">
-      <div className="card-body">
-        <h4 className="card-title">
-          Card idk
-        </h4>
-        <p className="card-text">  
-        </p> 
+function Cards({ cards, cardNum, setCardNum, flipped, setFlipped }) {
+  const cardJSX = cards.map((card) => {
+    return (
+      <div className="card mt-2">
+        <div className="card-body">
+          {card.front} {card.back}
+        </div>
       </div>
-    </div>
-  );
+    );
+  });
+
+  console.log(cards[cardNum]);
+  return <div>{cardJSX}</div>;
 }
 
 export default Cards;
