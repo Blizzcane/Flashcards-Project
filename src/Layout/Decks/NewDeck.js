49b28892d1cb7e8e03bd0ff364461fe4e0dd1543
Blizzcane <blizzcane@gmail.com>
2021-05-04 21:39:35 -0500

    buttons added to deckviewer

--- src/Layout/Decks/NewDeck.js
+++ src/Layout/Decks/NewDeck.js
@@ -12,7 +12,7 @@ function NewDeck() {
           <li class="breadcrumb-item active">Create Deck</li>
         </ol>
       </nav>
-      <h2>Create Deck</h2>
+      <h2 className="my-2">Create Deck</h2>
       <form>
         <label for="name">Name:</label>
         <input
@@ -21,6 +21,7 @@ function NewDeck() {
           required
           type="text"
           name="name"
+          style={{ width: "100%", marginBottom: "15px" }}
         />
         <label for="description">Description:</label>
         <textarea
@@ -28,11 +29,12 @@ function NewDeck() {
           placeholder="Brief description of the deck"
           required
           name="description"
+          style={{ width: "100%", marginBottom: "15px" }}
         ></textarea>
-        <Link to="/" class="btn btn-secondary">
+        <Link to="/" class="btn btn-secondary mr-2 mb-4">
           Cancel
         </Link>
-        <a href="#" class="btn btn-primary">
+        <a href="#" class="btn btn-primary  mb-4">
           Submit
         </a>
       </form>
