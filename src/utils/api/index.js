354740f4e9d1dda5bcee43e9207e53a7c1e0c0a2
Blizzcane <blizzcane@gmail.com>
2021-08-20 22:16:58 -0500

    still testing fixes

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -3,9 +3,11 @@ require("dotenv").config();
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-// const API_BASE_URL = process.env.API_BASE_URL || "https://flashcards-project-neon.vercel.app";
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
-console.log("API:", process.env.API_BASE_URL);
+const API_BASE_URL = process.env.API_BASE_URL || "https://flashcards-project-neon.vercel.app";
+// const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:5000";
+console.log("API_BASE_URL:", API_BASE_URL);
+console.log("process.env.NODE_ENV:", process.env.NODE_ENV);
+
 
 /**
  * Defines the default headers for these functions to work with `json-server`
